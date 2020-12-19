5914ddfb8dbe5bd9e49d0e57c128764e2db9d6b8
pray <comppro54@gmail.com>
2020-12-19 17:59:19 +0545

    models created

--- Database/main/models.py
+++ Database/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    EntryName= models.CharField(max_length=64)
+    Description=models.CharField(max_length=500)
+    Image=models.ImageField(upload_to='DataImages',blank=True,null=True)
+
+
+
+
